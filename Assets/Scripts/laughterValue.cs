cd26f34e5a61b441c22ee755116446a0d4b23b79
Mateusz Trocha <mtroc001@gold.ac.uk>
2024-01-27 21:42:30 +0000

    Added laugh-o-meter and shaders

--- Assets/Scripts/laughterValue.cs
+++ Assets/Scripts/laughterValue.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class laughterValue : MonoBehaviour
 {
-    public float sineValue;
+    //public float sineValue;
     public float scale;
     public float noise;
-    public float noiseScale = 1.0f;
 
-
+    private void Start()
+    {
+        scale = 1.0f;
+    }
     void Update()
     {
         // Update sineValue each frame to be the sine of the elapsed time
-        sineValue = Mathf.Sin(Time.time);
+        //sineValue = Mathf.Sin(Time.time);
 
         float x = Time.time * scale;
         float y = transform.position.y * scale;
 
         // Get a Perlin noise value (between 0 and 1)
-        noise = Mathf.PerlinNoise(x, y);
+        noise = Mathf.Clamp(1.5f*Mathf.PerlinNoise(x, y),0,1);
     }
 }
